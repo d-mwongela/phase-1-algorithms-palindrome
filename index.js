870f854fa4504929dd1f264cf15458bc520cff77
dennis00095 <dennis.mwongela@student.moringaschool.com>
2022-08-30 12:19:57 +0300

    Lab Completed

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+
+
+function reverse(word) {
+  
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = reverse(word);
+
+  return word === reversedWord;
 }
 
 /* 
-  Add your pseudocode here
-*/
 
-/*
-  Add written explanation of your solution here
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +25,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome(""));
 }
 
 module.exports = isPalindrome;
